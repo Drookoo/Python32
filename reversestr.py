477ad7040c0ea0a7c33c794fb414819837782f06
Andrew Ku <andrewku123@gmail.com>
2015-07-21 22:18:46 -0400

    Reverse-a-string
    reverses a string

---
+++ reversestr.py
@@ -0,0 +1,13 @@
+#doesnt work
+print("""
+Whalecum to my Reverse-a-string script.
+Input a string of words to begin.""")
+
+import string 
+
+sentence = input()
+
+words = int(len(sentence))  
+
+	
+print(sentence[words - 1])

b37bb4664ab7bd7df8ce54e82a1c3bae625541bf
Andrew Ku <andrewku123@gmail.com>
2015-07-29 13:51:33 -0400

    Mafia Roles Assigner
    Tool for deciding roles for the game Mafia

--- mafia.py
+++ mafia.py
@@ -4,10 +4,31 @@ The narrator uses this tool and discretely tells players on what thier
 roles are. This tool assumes you have 2 bad guys, 1 cop and an angel. 
 """
 
+import random
+
 print("Hi, enter the number of players in your game")
 players = input()
 
-print("Enter the names of the", players, "players")
-names = input()
-names.split
+x = 1
+playernames = []
 
+while x <= int(players):
+	print("Enter the name of player", x)
+	name = input() 
+	playernames.append(name)
+	x = x + 1
+
+print ("The following are the players:", playernames)
+random.shuffle(playernames)	
+
+print(playernames[0], "is the cop")
+playernames.remove(playernames[0])
+random.shuffle(playernames)	
+
+print(playernames[0], "is a member of the mafia")
+playernames.remove(playernames[0])
+random.shuffle(playernames)	
+print(playernames[0], "is a member of the mafia")
+playernames.remove(playernames[0])
+
+print(playernames, "are the citizens")

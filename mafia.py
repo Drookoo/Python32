eec4223c966b4cbb9d31db365eb80559443d9dc5
Andrew Ku <andrewku123@gmail.com>
2015-07-28 22:59:28 -0400

    Mafia roles assigner
    Tool for assigning roles in the game "Mafia"

---
+++ mafia.py
@@ -0,0 +1,13 @@
+""" 
+This is a tool for the game Mafia/ Werewolf/ or whatever you call yours.
+The narrator uses this tool and discretely tells players on what thier
+roles are. This tool assumes you have 2 bad guys, 1 cop and an angel. 
+"""
+
+print("Hi, enter the number of players in your game")
+players = input()
+
+print("Enter the names of the", players, "players")
+names = input()
+names.split
+

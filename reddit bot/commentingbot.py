5e7bfbd3d554222fa4851e3341867ec444fc9951
Andrew Ku <andrewku123@gmail.com>
2015-09-08 20:51:14 -0400

    self server bot

--- reddit bot/commentingbot.py
+++ reddit bot/commentingbot.py
@@ -1,12 +1,41 @@
 import praw
+import time
 r = praw.Reddit('subreddit commenting robo by /u/nachozombie v0.1')
 
 def startup():
 	r.login()
-	already_done = []
-	target()
 	
 def target():
+	x = 1
 	prawWords = []
-	if 
-	interests  = input("Enter the keywords or phrases that you want to look out for")
+	words = input("How many keywords or phrases are you looking for? ")
+	while x <= int(words):
+		print("Enter the keyword or phrase that you want to look out for. Phrase/Word #", x)
+		interests  = input()
+		prawWords.append(interests)
+		x = x + 1
+	
+def get_sub():
+	print("Subreddit: ")
+	sub = input()
+
+def get_comment():
+	print("Comment:")
+	comment = input()
+	
+def loop():	
+	already_done = []
+	while True:
+		subreddit = r.get_subreddit(input("Subreddit: "))
+		for submission in subreddit.get_new(limit = 5):
+			if submission.id not in already_done:
+				submission.add_comment(input("Comment: "))
+				already_done.append(submission.id)
+	time.sleep(15)			
+	
+def main():
+	startup()
+	target()
+	loop()
+	
+main()

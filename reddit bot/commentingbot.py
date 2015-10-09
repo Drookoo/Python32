4fd8f3c33a6562e921448ce15d2aed02f7b4e569
Andrew Ku <andrewku123@gmail.com>
2015-10-08 23:13:23 -0400

    New stuff. Read releases.txt for info

--- reddit bot/commentingbot.py
+++ reddit bot/commentingbot.py
@@ -1,10 +1,21 @@
 # if bot restarts, posts are not saved locally and bot will reply again
 import praw
 import time
+import os
+from bot_config import *
+
+# very very very small chance reddit banned this user agent :)
 r = praw.Reddit('subreddit commenting robo by /u/nachozombie v1.0')
 
 def startup():
-	r.login()
+	you_dun_goofed()
+	r.login(R_USERNAME, R_PASSWORD)
+	
+def you_dun_goofed():
+	if not os.path.isfile("bot_config.py"):
+		print("Looks like you don't know what you're doing.")
+		print("Make a config file for the bot. Name as bot_config.py")
+		exit(1)
 	
 def target():
 	x = 1
@@ -23,8 +34,24 @@ def get_subreddit():
 def get_comment():
 	response = input("Comment: ")
 	return response
-	
+
+def find_load_log():
+	if not os.path.isfile("already_done.txt"):
+		print("You thought you knew what you were doing.")
+		print("Make a log for the bot. Name as already_done.txt")
+	else:
+		with open("already_done.txt", "r") as f:
+			already_done = f.read()
+			already_done = already_done.split("\n")
+			already_done = filter(None, already_done)
+			
+def update_log():
+	with open("already_done.txt", "w") as f:
+		for post_id in already_done:
+			f.write(post_id + "\n")
+		
 def loop():	
+	find_load_log()
 	already_done = []
 	soob = get_subreddit()
 	comment = get_comment()
@@ -34,6 +61,8 @@ def loop():
 			if submission.id not in already_done:
 				submission.add_comment(comment)
 				already_done.append(submission.id)
+	update_log()			
+
 	time.sleep(15)			
 	
 def main():

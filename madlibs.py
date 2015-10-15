26df73e58294871494dd1343bfd9ec2cc7f0f31a
Andrew Ku <andrewku123@gmail.com>
2015-10-14 22:21:39 -0400

    use of random

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+import random
+
+def getnoun():
+    noun = input("Noun: ")
+   
+def getverb():
+    verb = input("Verb: ")
+    
+def getadj():
+    adj = input("Adjective: ")
+    
+def result():
+    print(text)
